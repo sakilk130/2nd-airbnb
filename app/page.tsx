7ba56ee886c0cb763cf157c12f394d206299fb8f
sakilk130 <sakilk130@gmail.com>
2023-04-19 14:29:07 +0600

    feat: home page empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+
 export default function Home() {
-  return <div>Hello AirBnb</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="sm:grid-flow-cols-2 md:grid-flow-cols-3 lg:grid-flow-cols-4 grid grid-cols-1 gap-8 pt-24 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>Test</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
