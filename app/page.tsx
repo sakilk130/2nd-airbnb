a20236817caff0f01cbad8a5d6d4e501c0b940a0
sakilk130 <sakilk130@gmail.com>
2023-04-19 15:18:18 +0600

    feat: show listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
+import getCurrentUser from './actions/getCurrentUser';
+import getListings from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
-export default function Home() {
-  const isEmpty = true;
-  if (isEmpty) {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
     );
   }
+
   return (
     <ClientOnly>
       <Container>
         <div className="sm:grid-flow-cols-2 md:grid-flow-cols-3 lg:grid-flow-cols-4 grid grid-cols-1 gap-8 pt-24 xl:grid-cols-5 2xl:grid-cols-6">
-          <div>Test</div>
+          {listings.map((listing: any) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
