8b9e54e71fa184bf387f1ec5db44dd7967bb12bd
sakilk130 <sakilk130@gmail.com>
2023-04-20 12:19:13 +0600

    fix:

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -7,7 +7,12 @@ export default async function getListings() {
         createdAt: 'desc',
       },
     });
-    return listings;
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      updatedAt: listing.updatedAt.toISOString(),
+    }));
+    return safeListings;
   } catch (error: any) {
     throw new Error(error);
   }
