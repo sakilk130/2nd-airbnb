0c553917e6bbab4eb52109b7f2cdae3e0ff5796d
sakilk130 <sakilk130@gmail.com>
2023-05-06 15:50:00 +0600

    feat: loading and error component

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-
-import EmptyState from '../components/EmptyState';
-import ClientOnly from '../components/ClientOnly';
 import getCurrentUser from '../actions/getCurrentUser';
 import getFavoritesListings from '../actions/getFavoritesListings';
+import ClientOnly from '../components/ClientOnly';
+import EmptyState from '../components/EmptyState';
 import FavoritesClient from './FavoritesClient';
-import ListingCard from '../components/listings/ListingCard';
 
 const FavoritesPage = async () => {
   const currentUser = await getCurrentUser();
@@ -23,15 +20,6 @@ const FavoritesPage = async () => {
   return (
     <ClientOnly>
       <FavoritesClient listings={listings} currentUser={currentUser} />
-      <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {listings.map((listing) => (
-          <ListingCard
-            key={listing.id}
-            currentUser={currentUser}
-            data={listing}
-          />
-        ))}
-      </div>
     </ClientOnly>
   );
 };
